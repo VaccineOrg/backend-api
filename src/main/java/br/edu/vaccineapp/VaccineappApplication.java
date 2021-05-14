b37c2ab4e76d4817145542b1cc2443e703be64e3
Bruno <brunoc.cavalcanti22@gmail.com>
2021-05-14 14:56:36 -0300

    first commit

---
+++ src/main/java/br/edu/vaccineapp/VaccineappApplication.java
@@ -0,0 +1,13 @@
+package br.edu.vaccineapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccineappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/vaccineapp/VaccineappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.vaccineapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

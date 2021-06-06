d134a8a4cc9f3088e503b861ada46e6b34a9b0e7
Bruno <brunoc.cavalcanti22@gmail.com>
2021-06-05 23:03:59 -0300

    Get user by email implementation

--- src/main/java/br/edu/vaccineapp/external/database/GetUserByEmailInDataBaseImpl.java
+++ src/main/java/br/edu/vaccineapp/external/database/GetUserByEmailInDataBaseImpl.java
@@ -1,22 +1,22 @@
 package br.edu.vaccineapp.external.database;
 
 import br.edu.vaccineapp.entity.User;
-import br.edu.vaccineapp.external.GetUserByIdInDataBase;
+import br.edu.vaccineapp.external.GetUserByEmailInDataBase;
 import br.edu.vaccineapp.external.database.entity.UserModel;
 import br.edu.vaccineapp.external.database.entity.adapter.UserModelAdapter;
-import br.edu.vaccineapp.external.database.repository.UserIdRepository;
+import br.edu.vaccineapp.external.database.repository.UserEmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class GetUserByIdInDataBaseImpl implements GetUserByIdInDataBase {
+public class GetUserByEmailInDataBaseImpl implements GetUserByEmailInDataBase {
 
     @Autowired
-    private UserIdRepository userIdRepository;
+    private UserEmailRepository userEmailRepository;
 
     @Override
-    public User execute(Long id) {
-        UserModel user = userIdRepository.findUserById(id);
+    public User execute(String email) {
+        UserModel user = userEmailRepository.findUserByEmail(email);
         return UserModelAdapter.modelToEntity(user);
     }
 }

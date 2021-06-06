5f7160dd0d5f9dc7877f28bf7769debabc17b174
Bruno <brunoc.cavalcanti22@gmail.com>
2021-06-06 15:48:38 -0300

    Login service

--- src/main/java/br/edu/vaccineapp/endpoint/rest/UserController.java
+++ src/main/java/br/edu/vaccineapp/endpoint/rest/UserController.java
@@ -2,8 +2,10 @@ package br.edu.vaccineapp.endpoint.rest;
 
 import br.edu.vaccineapp.entity.User;
 import br.edu.vaccineapp.usecase.creation.CreateUser;
+import br.edu.vaccineapp.usecase.validation.ValidateLogin;
 import br.edu.vaccineapp.viewmodel.UserVM;
 import br.edu.vaccineapp.viewmodel.adapter.UserVMAdapter;
+import br.edu.vaccineapp.viewmodel.input.LoginVM;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,6 +22,9 @@ public class UserController {
     @Autowired
     private CreateUser createUser;
 
+    @Autowired
+    private ValidateLogin validateLogin;
+
     @PostMapping("/register")
     @ApiOperation(value = "Create user in data base")
     @ResponseStatus(HttpStatus.CREATED)
@@ -29,4 +34,12 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
+    @PostMapping("/login")
+    @ApiOperation(value = "Login service, returns a user object json")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<UserVM> login(@RequestBody LoginVM loginVM) {
+        final User user = validateLogin.execute(loginVM);
+        final UserVM result = UserVMAdapter.entityToViewModel(user);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
 }

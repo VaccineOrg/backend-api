b81ad27aa6d464760894315feebc0734870e9b81
Bruno <brunoc.cavalcanti22@gmail.com>
2021-06-08 20:16:32 -0300

    Added repositories

---
+++ src/main/java/br/edu/vaccineapp/external/database/repository/UserVaccineCampaignUserIdRepository.java
@@ -0,0 +1,8 @@
+package br.edu.vaccineapp.external.database.repository;
+
+import br.edu.vaccineapp.external.database.entity.UserVaccineCampaignModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserVaccineCampaignUserIdRepository extends JpaRepository<UserVaccineCampaignModel, Long> {
+    UserVaccineCampaignModel findByUser_Id();
+}
